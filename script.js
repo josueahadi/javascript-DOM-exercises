c8cc742ed48fc52331dbfb488c88e9c8f25390ed
Josue <ahadihjosue@gmail.com>
2024-04-19 11:35:19 +0200

    getAttribute()

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-function changeAllArticleColors() {
-    const articles = document.querySelectorAll('#products article');
-    for (article of articles) {
-        article.classList.add('sale');
-        console.log(article.outerHTML);
-    }
+// function changeAllArticleColors() {
+//     const articles = document.querySelectorAll('#products article');
+//     for (article of articles) {
+//         article.classList.add('sale');
+//         console.log(article.outerHTML);
+//     }
     
+// }
+// changeAllArticleColors();
+
+
+function attachBuyEvents() {
+    const buttons = document.querySelectorAll('#products button');
+    
+    for (let button of buttons) {
+        button.addEventListener('click', function(event){
+            //console.log('BUY!')
+            this.parentElement.classList.toggle('sale')
+            console.log(this.parentElement.getAttribute('data-id'))
+        });
+    }
+
+    // const articles = document.querySelectorAll('#products article');
+
+    // for (let article of articles) {
+    //     article.addEventListener('click', function(){
+    //         console.log(this.getAttribute('data-id'));
+    //     });
+    // }
 }
-changeAllArticleColors();
+
+attachBuyEvents();
